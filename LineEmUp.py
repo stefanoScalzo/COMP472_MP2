14f1a009fcefbf2ec9b4d083bca460be5bbb786f
Matthew Mileto <mileto.matthew@gmail.com>
2021-11-14 04:53:54 -0500

    "merge should be successfully completed. need to confirm with Tahar tomorrow."

--- LineEmUp.py
+++ LineEmUp.py
@@ -1,11 +1,10 @@
-#!/usr/bin/env python
+# !/usr/bin/env python
 # coding: utf-8
 
 # based on code from https://stackabuse.com/minimax-and-alpha-beta-pruning-in-python
 
 import time
 import random
-import sys
 from PrintManager import PrintManager
 
 
@@ -38,6 +37,7 @@ class LineEmUp:
         :param a1:
         :param a2:
         """
+
         self.board_size = board_size
         self.blocks = blocks
         self.blocks_coord = blocks_coord
@@ -64,6 +64,7 @@ class LineEmUp:
         """
 
         # variables required for stats
+        self.move_counter = 0
         self.heuristic_times = []
         self.total_heuristic_times = []
         self.state_count = 0
@@ -79,8 +80,7 @@ class LineEmUp:
 
     def initialize_board(self):
         """
-        Creates the board by initialing all empty tokens, the block tokens, and starting player.
-
+        Creates the board: initializes all empty tokens, the block tokens, and sets turn player.
         :return:
         """
         self.current_state = []
@@ -95,7 +95,7 @@ class LineEmUp:
         # set Block nodes
         self.initialize_blocks(self.blocks, self.blocks_coord)
 
-        # Player W always plays first
+        # Player White always plays first
         self.player_turn = 'W'
         self.heuristic = self.heuristic_w
         self.max_depth = self.d1
@@ -104,7 +104,6 @@ class LineEmUp:
     def initialize_blocks(self, blocks, blocks_coord):
         """
         Initializes blocks on the board.
-
         :param blocks: number of blocks that should be on board
         :param blocks_coord: coordinates for the blocks
         :return:
@@ -115,23 +114,21 @@ class LineEmUp:
         for coord in blocks_coord:
             self.current_state[coord[0]][coord[1]] = 'x'
 
-    def draw_board(self):
+    def draw_board(self, printer):
         """
         Prints the board to the console
-
         :return:
         """
-        print()
+        printer.write("\n")
         for x in range(0, self.board_size):
             for y in range(0, self.board_size):
-                print(F'{self.current_state[x][y]}', end="  ")
-            print()
-        print()
+                printer.write(F'{self.current_state[x][y]}    ')
+            printer.write("\n")
+        printer.write("\n")
 
     def is_valid_play(self, x, y):
         """
         Checks that the provided coordinates x, y would be a valid move for a player to make
-
         :param x:
         :param y:
         :return: true or false
@@ -146,7 +143,6 @@ class LineEmUp:
     def valid_coord(self, x, y):
         """
         Validates that the provided coordinates are within the legal bounds of the board
-
         :param x:
         :param y:
         :return: true or false
@@ -162,14 +158,12 @@ class LineEmUp:
             - A player has n consecutive tokens in a vertical column
             - A player has n consecutive tokens in a diagonal
             - The board is full (i.e. a tie)
-
         Returns any of the following:
             - W if White is the winner
             - B if Black is the winner
             - . if it's a tie
             - None if the game is not over
-
-        :return: the winner (X,O, or . for tie) or None
+        :return: the winner (W,B, or . for tie) or None
         """
 
         # Check for winner
@@ -246,7 +240,6 @@ class LineEmUp:
         """
         Displays a user-friendly message indicating the winner of the game (if any)
         If game is not over, simply returns value of is_end()
-
         :return: winner or value of is_end()
         """
         self.result = self.is_end()
@@ -293,15 +286,14 @@ class LineEmUp:
 
     def minimax(self, current_depth=0, max_turn=False):
         """
-        Minimizing for 'White' and maximizing for 'Black'
+        Minimizing for 'W' and maximizing for 'B'
         Possible values are:
-            -101 - win for 'White'
+            -inf - win for 'W'
               0  - a tie
-            101  - loss for 'White'
-
+            inf  - loss for 'W'
         :param current_depth: required to determine when to stop traversing
         :param max_turn: boolean to decide which computation will be used (max or min)
-        :return: the value of the heuristic being propagated along with the X,Y coordinates of the best computed move
+        :return: the value of the heuristic being propagated along with the x,y coordinates of the best computed move
         """
 
         value = 101
@@ -310,18 +302,16 @@ class LineEmUp:
         x = None
         y = None
 
-        ard = []
-
         current_depth = current_depth + 1
 
-        # Iterate through every possible move (i, j)
         rows = [*range(0, self.board_size)]
         random.shuffle(rows)
         cols = [*range(0, self.board_size)]
         random.shuffle(cols)
 
-        for i in rows:
-            for j in cols:
+        # Iterate through every possible move (i, j)
+        for i in range(0, self.board_size):
+            for j in range(0, self.board_size):
 
                 # Only consider valid moves (i.e. cells that are empty)
                 if self.current_state[i][j] == '.':
@@ -338,7 +328,6 @@ class LineEmUp:
                         if end_of_traversal or self.is_end() or self.timer_is_up:
                             self.depths.append(current_depth)
                             self.ard_per_move.append(current_depth)
-
                             if self.heuristic == self.E1:
                                 v = self.e()
                             else:
@@ -354,13 +343,12 @@ class LineEmUp:
                         if end_of_traversal or self.is_end() or self.timer_is_up:
                             self.depths.append(current_depth)
                             self.ard_per_move.append(current_depth)
-
                             if self.heuristic == self.E1:
                                 v = self.e()
                             else:
                                 v = self.e2(current_depth)
-
                         else:
+                            # current_depth = current_depth + 1
                             (v, _, _) = self.minimax(current_depth, max_turn=True)
                         if v < value:
                             value = v
@@ -373,23 +361,21 @@ class LineEmUp:
                     # Reset cell so that state is not permanently modified by A.I. traversal
                     self.current_state[i][j] = '.'
 
-                    if self.timer_is_up:
+                    if (self.timer_is_up):
                         return value, x, y
 
-        self.ard_per_move[0] = sum(self.ard_per_move) / len(self.ard_per_move)
-        self.ard_per_move = self.ard_per_move[0:]
         # add state count to depth
         if current_depth not in self.state_count_p_depth:
             self.state_count_p_depth[current_depth] = 0
 
         self.state_count_p_depth[current_depth] += self.state_count
 
+        # print("LEAVING STATE SO NOT IN INFINITE LOOP WOO!")
         return value, x, y
 
     def alphabeta(self, current_depth=0, alpha=-100, beta=100, max_turn=False):
         """
         A more efficient version of minimax (i.e. includes pruning)
-
         :param current_depth: required to determine when to stop traversing
         :param alpha: required for pruning
         :param beta: required for pruning
@@ -423,6 +409,11 @@ class LineEmUp:
                     if time.time() - self.move_start > self.max_move_time:
                         self.timer_is_up = True
 
+                    # If timer has expired, stop traversing and set the current depth as max depth
+                    # if not self.timer_is_up and move_time >= self.max_move_time :
+                    #     self.max_depth_adjusted = current_depth
+                    #     self.timer_is_up = True
+
                     # end traversal if resources are spent
                     end_of_traversal = current_depth == self.max_depth
 
@@ -454,6 +445,7 @@ class LineEmUp:
                                 v = self.e2(current_depth)
 
                         else:
+                            # current_depth = current_depth + 1
                             (v, _, _) = self.alphabeta(current_depth, alpha, beta, max_turn=True)
                         if v < value:
                             value = v
@@ -466,7 +458,7 @@ class LineEmUp:
                     # Reset cell so that state is not permanently modified by A.I. traversal
                     self.current_state[i][j] = '.'
 
-                    if self.timer_is_up:
+                    if (self.timer_is_up):
                         return value, x, y
 
                     # Prune unnecessary siblings
@@ -507,7 +499,7 @@ class LineEmUp:
 
             if count_b == self.winning_size:
                 score = score + 2
-            if count_w >= self.winning_size / 2:
+            if count_b >= self.winning_size / 2:
                 score = score + 1
             else:
                 score = score + 1
@@ -518,17 +510,15 @@ class LineEmUp:
 
     def e2(self, current_depth):
         """
-        More complicated heuristic which will check all winning situations for X and O
-        and return the difference in terms of O (i.e. O - X).
-
+        More complicated heuristic which will check all winning situations for W and B
+        and return the difference in terms of O (i.e. B - W).
         The current depth is required to determine how close the winning or losing situation is
         (i.e., a loss in current turn should be prioritized over a loss in three turns)
-
         :param current_depth:
-        :return:    - +101 if O wins
-                    - -101 if X wins
+        :return:    - +101 if Black wins
+                    - -101 if Black loses
                     - 0 if a tie
-                    - otherwise, winning situations for O minus winning situations for X
+                    - otherwise, winning situations for Black minus winning situations for White
         """
 
         start = time.time()
@@ -543,7 +533,7 @@ class LineEmUp:
         if self.is_end() == '.':
             return 0
 
-        # calculate score otherwise (i.e. scenarios O is winning minus scenarios X is winning)
+        # calculate score otherwise (i.e. scenarios B is winning minus scenarios W is winning)
         for i in range(0, self.board_size):
             for j in range(0, self.board_size):
 
@@ -568,9 +558,9 @@ class LineEmUp:
 
                 # increment winning score accordingly
                 if streak == self.winning_size:
-                    if winner == 'X':
+                    if winner == 'W':
                         winning_w += 1
-                    elif winner == 'O':
+                    elif winner == 'B':
                         winning_b += 1
                     elif winner == '.':
                         winning_w += 1
@@ -655,46 +645,78 @@ class LineEmUp:
 
         return winning_b - winning_w
 
-
-    def printInitialGame(self):
+    def printInitialGame(self, printer):
         """
         Prints stats pertaining to the beginning of the game such as
         board size, blocks, number of nodes needed in a row to win, etc.
-
         :return:
         """
-
-        print("n: " + str(self.board_size))
-        print("b: " + str(self.blocks))
-        print("s: " + str(self.winning_size))
-        print("t: " + str(self.max_move_time))
+        printer.write("n: " + str(self.board_size) + '\n')
+        printer.write("b: " + str(self.blocks) + '\n')
+        printer.write("s: " + str(self.winning_size) + '\n')
+        printer.write("t: " + str(self.max_move_time) + '\n')
+        printer.write("d1: " + str(self.d1) + '\n')
+        printer.write("d2: " + str(self.d2) + '\n')
         if (self.player_w == self.AI):
-            print("Player White: AI")
+            printer.write("Player White: AI" + '\n')
         else:
-            print("Player White: Human")
+            printer.write("Player White: Human" + '\n')
 
         if (self.player_b == self.AI):
-            print("Player Black: AI")
+            printer.write("Player Black: AI" + '\n')
         else:
-            print("Player Black: Human")
-
+            printer.write("Player Black: Human" + '\n')
         if (self.a1 == self.ALPHABETA):
-            print("Algo for White: ALPHABETA")
+            printer.write("Algo for White: ALPHABETA" + '\n')
         else:
-            print("Algo for White: MINIMAX")
+            printer.write("Algo for White: MINIMAX" + '\n')
         if (self.a2 == self.ALPHABETA):
-            print("Algo for Black: ALPHABETA")
+            printer.write("Algo for Black: ALPHABETA" + '\n')
         else:
-            print("Algo for Back: MINIMAX")
+            printer.write("Algo for Black: MINIMAX" + '\n')
 
         if (self.heuristic_w == self.E1):
-            print("Player White heuristic: E1")
+            printer.write("Player White heuristic: E1" + '\n')
         else:
-            print("Player White heuristic: E2")
+            printer.write("Player White heuristic: E2" + '\n')
         if (self.heuristic_b == self.E1):
-            print("Player Black heuristic: E1")
+            printer.write("Player Black heuristic: E1" + '\n')
         else:
-            print("Player Black heuristic: E2")
+            printer.write("Player Black heuristic: E2" + '\n')
+
+    def printIntialGameToFile(self, file):
+        file.write("n: " + str(self.board_size) + '\n')
+        file.write("b: " + str(self.blocks) + '\n')
+        file.write("s: " + str(self.winning_size) + '\n')
+        file.write("t: " + str(self.max_move_time) + '\n')
+        file.write("d1: " + str(self.d1) + '\n')
+        file.write("d2: " + str(self.d2) + '\n')
+        if (self.player_w == self.AI):
+            file.write("Player White: AI" + '\n')
+        else:
+            file.write("Player White: Human" + '\n')
+
+        if (self.player_b == self.AI):
+            file.write("Player Black: AI" + '\n')
+        else:
+            file.write("Player Black: Human" + '\n')
+        if (self.a1 == self.ALPHABETA):
+            file.write("Algo for White: ALPHABETA" + '\n')
+        else:
+            file.write("Algo for White: MINIMAX" + '\n')
+        if (self.a2 == self.ALPHABETA):
+            file.write("Algo for Black: ALPHABETA" + '\n')
+        else:
+            file.write("Algo for Black: MINIMAX" + '\n')
+
+        if (self.heuristic_w == self.E1):
+            file.write("Player White heuristic: E1" + '\n')
+        else:
+            file.write("Player White heuristic: E2" + '\n')
+        if (self.heuristic_b == self.E1):
+            file.write("Player Black heuristic: E1" + '\n')
+        else:
+            file.write("Player Black heuristic: E2" + '\n')
 
     def getStats(self):
         """
@@ -714,22 +736,21 @@ class LineEmUp:
         printer = PrintManager()
         printer.setPath(F'gameTrace-{self.board_size}{self.blocks}{self.winning_size}{self.max_move_time}.txt')
 
-        self.printInitialGame()
-        game_over = False
-        self.move_counter = 0
-        while not game_over:
-            self.draw_board()
+        self.printInitialGame(printer)
+
+        while True:
+            self.draw_board(printer)
             if self.check_end():
-                game_over = True
-                print('i. Average evaluation time of heuristic: ' + str(
-                    1.0 * sum(self.total_heuristic_times) / len(self.total_heuristic_times)))
-                print('ii. Total states evaluated: ' + str(self.total_state_counts))
-                print('iii. Average of average depths: ' + str(sum(self.depth_averages) / len(self.depth_averages)))
-                print('iv. Total number of states evaluated at each depth: ')
+                printer.write('i. Average evaluation time of heuristic: ' + str(
+                    1.0 * sum(self.total_heuristic_times) / len(self.total_heuristic_times)) + '\n')
+                printer.write('ii. Total states evaluated: ' + str(self.total_state_counts) + '\n')
+                printer.write('iii. Average of average depths: ' + str(
+                    sum(self.depth_averages) / len(self.depth_averages)) + '\n')
+                printer.write('iv. Total number of states evaluated at each depth: \n')
                 for depth in sorted(self.total_state_counts_p_depth.keys(), reverse=True):
-                    print("\t" + str(depth) + ": " + str(self.total_state_counts_p_depth[depth]))
-                print('v. Average ARD: ' + str(sum(self.ard_averages) / len(self.ard_averages)))
-                print('vi. Total Move Count: ' + str(self.move_counter))
+                    printer.write("\t" + str(depth) + ": " + str(self.total_state_counts_p_depth[depth]) + '\n')
+                printer.write('v. Average ARD: ' + str(sum(self.ard_averages) / len(self.ard_averages)) + '\n')
+                printer.write('vi. Total Move Count: ' + str(self.move_counter) + '\n')
                 return
 
             self.move_start = time.time()
@@ -750,24 +771,24 @@ class LineEmUp:
             eval_time = round(end - self.move_start, 7)
             self.move_counter += 1
 
-            print('Heuristic returned: ' + str(v))
-            print(F'i. Evaluation time: {eval_time}s')
-            print('i. Heuristic evaluation time: ' + str(sum(self.heuristic_times)))
-            print("ii. Number of states evaluated: " + str(self.state_count))
-            print("iii. Number of states evaluated per depth:")
+            printer.write('Heuristic returned: ' + str(v) + '\n')
+            printer.write(F'i. Evaluation time: {eval_time}s')
+            printer.write('i. Heuristic evaluation time: ' + str(sum(self.heuristic_times)) + '\n')
+            printer.write("ii. Number of states evaluated: " + str(self.state_count) + '\n')
+            printer.write("iii. Number of states evaluated per depth:")
             for depth in sorted(self.state_count_p_depth.keys(), reverse=True):
-                print("\t" + str(depth) + ": " + str(self.state_count_p_depth[depth]))
-            print("iv. Average depths of heuristic evaluation: " + str(
-                round(1.0 * sum(self.depths) / len(self.depths), 4)))
-            print("v. ARD: " + str(self.ard_per_move[0]))
+                printer.write("\t" + str(depth) + ": " + str(self.state_count_p_depth[depth]) + '\n')
+            printer.write("iv. Average depths of heuristic evaluation: " + str(
+                round(1.0 * sum(self.depths) / len(self.depths), 4)) + '\n')
+            printer.write("v. ARD: " + str(self.ard_per_move[0]) + '\n')
             if (self.player_turn == 'W' and self.player_w == self.HUMAN) or (
                     self.player_turn == 'B' and self.player_b == self.HUMAN):
                 if self.recommend:
-                    print(F'Recommended move: x = {x}, y = {y}')
+                    print(F'Recommended move: x = {x}, y = {y}') + '\n'
                 (x, y) = self.input_move()
             elif (self.player_turn == 'W' and self.player_w == self.AI) or (
                     self.player_turn == 'B' and self.player_b == self.AI):
-                print(F'Player {self.player_turn} under AI control plays: x = {x}, y = {y}')
+                print(F'Player {self.player_turn} under AI control plays: x = {x}, y = {y}' + '\n')
 
             # store stat variables before resetting
             self.total_heuristic_times.extend(self.heuristic_times)
@@ -784,6 +805,7 @@ class LineEmUp:
             self.state_count = 0
             self.state_count_p_depth = {}
             self.depths = []
+            self.max_depth_adjusted = self.max_depth
             self.timer_is_up = False
             self.current_state[x][y] = self.player_turn
             self.ard_per_move = []
